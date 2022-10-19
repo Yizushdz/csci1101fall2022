8a94d2c09eca5f2299985ca1f4e9e731a1873276
Jesus Hernandez <jesushdz2002ch14@gmail.com>
2022-10-19 13:35:21 -0500

    Finished project

--- Hangman/main.py
+++ Hangman/main.py
@@ -15,7 +15,53 @@ for current_answer_character in answer:
 
 # Game Logic
 current_incorrect_guesses = 0
+letters_guessed = []
 
-while current_incorrect_guesses < num_of_incorrect_guesses or False not in answer_guessed:
+while current_incorrect_guesses < num_of_incorrect_guesses and False in answer_guessed:
+    # game summary.
     print(f"Number of incorrect guesses left: {num_of_incorrect_guesses - current_incorrect_guesses}")
+    print("Guessed letters: ", end = "")
+    for current_guessed_letter in letters_guessed:
+        print(current_guessed_letter, end=" ")
+    
+    print()
+
     # display puzzle board.
+    for current_answer_index in range(len(answer)):
+        if answer_guessed[current_answer_index]:
+            print(answer[current_answer_index], end="")
+        else:
+            print("_", end="")
+    
+    print()
+
+    # let user guess a letter
+    letter = input("Enter a letter: ")
+
+    letter = letter.upper()
+
+    print()
+    # Check if user entered a valid letter. 
+    if re.search("^[A-Z]$", letter) and len(letter) == 1 and letter not in letters_guessed:
+        current_letter_index = 0
+
+        # insert the letter in list of letter guesses (insertion sort)
+        for current_letter_guessed in letters_guessed:
+            if letter < current_letter_guessed:
+                break
+            current_letter_index += 1
+        letters_guessed.insert(current_letter_index, letter)
+
+        # See if letter is in the answer.
+        if letter in answer:
+            for current_answer_index in range(len(answer)):
+                if letter == answer[current_answer_index]:
+                    answer_guessed[current_answer_index] = True
+        else:
+            current_incorrect_guesses += 1
+
+# Post-game summary 
+if current_incorrect_guesses < num_of_incorrect_guesses:
+    print("Congrats, you won!!")
+else:
+    print(f"Sorry, you lost. The answer was {answer}")
