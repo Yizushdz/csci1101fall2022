87b5206863df3eaf7596fecc1153a58fb09f4664
Jesus Hernandez <jesushdz2002ch14@gmail.com>
2022-11-21 13:31:07 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+SELECT * FROM sandbox;
+
+--INSERT INTO sandbox(int32_value, bool_value, double_value) VALUES (37728, 1, 48463.23847);
+
+--UPDATE sandbox SET string_value = 'no' WHERE id = 3
+
+--DELETE FROM sandbox WHERE id = 1;
